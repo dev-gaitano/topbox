b5463c61a67bbd206454121020bdd6f87971c3ef
gaitano <dev.gaitano@gmail.com>
2026-04-13 11:28:42 +0300

    feat: implemented toggle between single and rollout content creation
    - Added state to `ContentManagement` to switch between creation modes.
    - Integrated `RolloutContentCreation` component.
    - Updated styles for the mode selector.

--- frontend/src/components/ContentManagement/ContentManagement.tsx
+++ frontend/src/components/ContentManagement/ContentManagement.tsx
@@ -1,7 +1,21 @@
 import "./ContentManagement.css"
 import SingleContentCreation from "./SingleContentCreation"
+import RolloutContentCreation from "./RolloutContentCreation"
+import { useState, useEffect } from "react"
 
 function ContentManagement() {
+  const [contentCreationMode, setContentCreationMode] = useState(0)
+
+  // true(1) represents Rollout Content Creation mode
+  // false(0) represents Single Content Creation mode
+  function handleContentCreationMode() {
+    if (contentCreationMode == 0) {
+      setContentCreationMode(1)
+    } else {
+      setContentCreationMode(0)
+    }
+  }
+
   return (
     <section className="content-management component">
       <div className="section-title">
@@ -11,18 +25,25 @@ function ContentManagement() {
       <div className="cm-container">
         <div className="cm-saved-content"></div>
         <div className="cm-content">
-          <div className="cm-selector">
-            <button className="btn-secondary">
-              <p>Single content creation</p>
-            </button>
-            <button className="btn-secondary inactive">
-              <p>Rollout content creation</p>
+          <div className={`cm-selector ${contentCreationMode ? "set-right" : "set-left"}`}>
+            <button className="btn-secondary" onClick={handleContentCreationMode}>
+              {contentCreationMode ? (
+                <p>Rollout content creation</p>
+              ) : (
+                <p>Single content creation</p>
+              )}
             </button>
           </div>
-          <SingleContentCreation />
+          <div>
+            {contentCreationMode ? (
+              <RolloutContentCreation />
+            ) : (
+              <SingleContentCreation />
+            )}
+          </div>
         </div>
       </div>
-    </section>
+    </section >
   )
 }
 
